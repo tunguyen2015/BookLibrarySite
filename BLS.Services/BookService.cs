91fb5b00d1b3ee927a3d2a31ab6729d2ea12c190
Nguyen Trung Tu <williamtunguyen2015@gmail.com>
2017-05-18 18:05:32 +0700

    Second commit - Back-end Added & Completed.

---
+++ BLS.Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BLS.Core.Data;
+using BLS.Data.Repository;
+using BLS.Data.UnitOfWork;
+
+namespace BLS.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IRepository<Book> _bookRepository;
+
+        public BookService(IUnitOfWork unitOfWork)
+        {
+            _bookRepository = unitOfWork.Repository<Book>();
+        }
+
+        public async Task<ICollection<Book>> GetAll()
+        {
+            return await _bookRepository.GetAll();
+        }
+
+        public async Task<Book> GetById(object id)
+        {
+            return await _bookRepository.GetById(id);
+        }
+
+        public async Task<Book> Add(Book books)
+        {
+            return await _bookRepository.Add(books);
+        }
+
+        public async Task<Book> Edit(Book books, int key)
+        {
+            return await _bookRepository.Edit(books, key);
+        }
+
+        public async Task<int> Delete(Book books)
+        {
+            return await _bookRepository.Delete(books);
+        }
+    }
+}
---
+++ BLS.Services/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLS.Core.Data;
+
+namespace BLS.Services
+{
+    public interface IBookService
+    {
+        Task<ICollection<Book>> GetAll();
+        Task<Book> GetById(object id);
+        Task<Book> Add(Book books);
+        Task<Book> Edit(Book books, int key);
+        Task<int> Delete(Book books);
+    }
+}
